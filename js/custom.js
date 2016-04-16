4ca584ac4aecc8581aa72e6cbe297d99b68d865d
Luke <Lukey@Lukaszs-MBP.home>
2016-04-16 23:34:38 +0100

    fadein change

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,22 @@
-// var $el = $(".heading:first"), text = $el.text(),
-//     words = text.split(" ");
 
-// var html = "";
-// for (var i = 0; i < words.length; i++) {
-//     html += "<span>" + words[i] + " </span>";
-// };
 
-// $el.html(html).children().css('opacity', '0').each(function(i){
-//   $(this).delay(i*200).fadeTo(100,1)
-// });
+
+$(document).ready(function() { 
+    
+
+	var $el = $(".heading:first"), text = $el.text(),
+    words = text.split(" ");
+
+	var html = "";
+	for (var i = 0; i < words.length; i++) {
+	    html += "<span>" + words[i] + " </span>";
+	};
+
+	$el.html(html).children().css('opacity', '0').each(function(i){
+	  $(this).delay(i*100).fadeTo(2000,1)
+	});
+
+
+
+});
 
