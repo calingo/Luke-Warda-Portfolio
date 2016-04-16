e432702dfd8df901f148b26d8dddf17d3e301dae
Luke <Lukey@Lukaszs-MBP.home>
2016-04-16 23:21:53 +0100

    styling and content test

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// var $el = $(".heading:first"), text = $el.text(),
+//     words = text.split(" ");
+
+// var html = "";
+// for (var i = 0; i < words.length; i++) {
+//     html += "<span>" + words[i] + " </span>";
+// };
+
+// $el.html(html).children().css('opacity', '0').each(function(i){
+//   $(this).delay(i*200).fadeTo(100,1)
+// });
+
--- projects/project3/project3.md
+++ projects/project3/project3.md
@@ -3,5 +3,5 @@ layout: default
 type: project
 folder: project3
 title: Burberry In-Store Tablet
-subtitle: Seamless online browsing in-store
+subtitle: Seamless online browsing in-store and store assistant tool
 ---
